95ac151c1a890e50539df4ee048c8d178d6f6ae5
Andreas Gkotsopoulos <gotsos42@gmail.com>
2026-01-10 21:34:21 +0200

    Bump version to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         targetSdk = 35
 
         // ✅ ΑΛΛΑΓΗ: Ανεβάσαμε τον κωδικό έκδοσης για να το δεχτεί το Play Console
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
